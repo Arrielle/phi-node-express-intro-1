419e6b2401d7de3707cc2d07e7ad56771d05394a
Arrielle <arrielle.kooiman@gmail.com>
2017-02-21 17:44:10 -0600

    adding comments

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
+var express = require('express'); //this app.js requires use of the express 'module' store the 'link' to this module in 'things' below
+var app = express(); //not 100% on this. express is a function that is stored in app, but I don't know what that function is
+var bodyParser = require('body-parser'); //this app.js requires use of the express 'module' store the 'link' to this module in 'things' below
 
-app.use(express.static('public'));
+app.use(express.static('public')); // calls the express() function uses it checks to see if the requested 'thing' is static, if it is, it looks in the public folder
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: true}));//bodyParser checks to see if the 'thing' is url encoded? If so, runs the function inside of body-parser?
 
-var songList = [
+var songList = [ //created starter songs
   {
     title: 'We did not start the Phire',
     artist: 'Billy Joel'
@@ -17,19 +17,19 @@ var songList = [
   }
 ];
 
-app.get('/songs', function(req, res) {
-  res.send(songList);
+app.get('/songs', function(req, res) { //if there is a get request AND the get request is looking for /songs url we run the function
+  res.send(songList); //we respond by sending teh songList Array
 });
 
-app.post('/newSong', function(req, res){
-  var newSong = req.body;
-  if(newSong.artist !== "Justin Bieber"){
-    songList.push(newSong);
-    console.log(songList);
-    res.sendStatus(200);
+app.post('/newSong', function(req, res){ //if there is a post request AND the request is looking in /newSong we run the function
+  var newSong = req.body; //we set newSong to req.body the req.body is DATA so newSong is the 'data' that was stored in our post request and that is equal to our newSongObject that the users created with the inputs
+  if(newSong.artist !== "Justin Bieber"){ //if the new song added DOES NOT HAVE Justin Bieber as the artist, the song should be successfully added
+    songList.push(newSong); //push newSong/req.body/data/newSongObject to the songListArray
+    console.log(songList); //logging to make sure it works
+    res.sendStatus(200); //success!
   } else {
-    res.sendStatus(500);
+    res.sendStatus(500); //ERROR (damn you Justin Bieber)
   }
 });
 
-app.listen(3000);
+app.listen(3000); //listen runs once on load and continues running
